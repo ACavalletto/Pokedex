7fde505b9b970b51bf7e5319dc1303ac9a329d44
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2022-05-19 13:58:55 -0600

    Completed edit route started new route

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+const methodOverride = require("method-override")
 const pokemon = require('./models/pokemon')
 
 app.use(express.urlencoded({ extended: false }))
@@ -11,11 +11,29 @@ app.get("/", (req, res) => {
     res.render("index.ejs", {pokemon:pokemon})
 })
 
-app.get("/:id", (req, res) => {
-    res.render("show.ejs")
+app.get("/new", (req, res) => {
+    res.render("new.ejs")
 })
 
+app.get("/:id/edit", (req, res) => {
+    const { stats } = pokemon[req.params.id]
+    res.render("edit.ejs", {stats, id: req.params.id})
+})
 
+app.get("/:id", (req, res) => {
+    const { id, name, img, type, stats, moves, damages, misc } = pokemon[req.params.id];
+    res.render("show.ejs", {id,name,img, type, stats, moves, damages, misc, index:req.params.id})
+})
+
+app.post("/", (req, res) => {
+    pokemon.push(req.body)
+    res.redirect("/")
+})
+
+app.put("/:id", (req, res) => {
+    pokemon[req.params.id].stats = req.body
+    res.redirect(`/${req.params.id}`)
+})
 
 app.listen(port, () => {
     console.log("Listening on port: " + port)
